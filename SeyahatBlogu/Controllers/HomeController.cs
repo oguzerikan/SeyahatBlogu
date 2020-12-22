7749655c5c006035e66bc462c64dfaf8d833d5c9
Oğuz Kaan Erikan <dokuz.oguz@hotmail.com>
2020-12-22 17:08:46 +0300

    Hakkımızda Sayfası Verileri Listeleme

--- SeyahatBlogu/Controllers/HomeController.cs
+++ SeyahatBlogu/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeyahatBlogu.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,17 @@ namespace SeyahatBlogu.Controllers
 {
     public class HomeController : Controller
     {
+        Context c = new Context();
         public ActionResult Index()
-        {
+        {            
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
         }
 
         public ActionResult Contact()
