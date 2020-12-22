2e535af6f90dc08efcb0cef6f43def2293daec2d
Oğuz Kaan Erikan <dokuz.oguz@hotmail.com>
2020-12-22 12:28:29 +0300

    Sınıflar Klasöründe Code First Sınıfları Oluşturuldu.

---
+++ SeyahatBlogu/Models/Siniflar/Yorumlar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SeyahatBlogu.Models.Siniflar
+{
+    public class Yorumlar
+    {
+        [Key]
+        public int ID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Mail { get; set; }
+        public string Yorum { get; set; }
+        public int BLOGID { get; set; }
+    }
+}
