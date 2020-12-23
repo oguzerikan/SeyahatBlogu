d30add9371cd21583703503e6b9b1b959af5aa4e
Oğuz Kaan Erikan <dokuz.oguz@hotmail.com>
2020-12-23 16:19:29 +0300

    Blog/Index sayfasında blog listesi oluşturuldu.

--- SeyahatBlogu/Controllers/BlogController.cs
+++ SeyahatBlogu/Controllers/BlogController.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SeyahatBlogu.Models.Siniflar;
 using System.Web.Mvc;
 
 namespace SeyahatBlogu.Controllers
 {
     public class BlogController : Controller
     {
+        Context c = new Context();
         // GET: Blog
         public ActionResult Index()
         {
-            return View();
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
         }
     }
 }
