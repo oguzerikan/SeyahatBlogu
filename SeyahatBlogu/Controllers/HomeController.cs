2bbd5ca8482127c7472981f301569c40a4b7d7b0
Oğuz Kaan Erikan <dokuz.oguz@hotmail.com>
2020-12-28 14:48:33 +0300

    Ana sayfada, en beğendiğim yerler bölümünde sol sütunu ayarladık.

--- SeyahatBlogu/Controllers/HomeController.cs
+++ SeyahatBlogu/Controllers/HomeController.cs
@@ -48,5 +48,11 @@ namespace SeyahatBlogu.Controllers
             var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(10).ToList();
             return PartialView(degerler);
         }
+
+        public PartialViewResult Partial4()
+        {
+            var degerler = c.Blogs.Take(3).ToList();
+            return PartialView(degerler);
+        }
     }
 }
