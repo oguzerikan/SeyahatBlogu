3fbe10e0bd43df3192556c7bb67be2c3c6193ce1
Oğuz Kaan Erikan <dokuz.oguz@hotmail.com>
2020-12-29 13:56:21 +0300

    Admin/Index sayfasına blog listesini getirdik.

---
+++ SeyahatBlogu/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SeyahatBlogu.Models.Siniflar;
+
+namespace SeyahatBlogu.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+    }
+}
